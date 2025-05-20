bc9783a7bf2c7e030b54e28bf40914357c0a7f55
Codemeister14 <122707000+Codemeister14@users.noreply.github.com>
2025-05-20 13:13:07 -0700

    Create readpower.py

---
+++ hep-score-v2.0rc6/hepscore/readpower.py
@@ -0,0 +1,34 @@
+import subprocess
+import requests
+import base64
+
+token = "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"
+
+def get_dell_serial_linux():
+    try:
+        result = subprocess.run(
+            ["sudo", "dmidecode", "-s", "system-serial-number"],
+            capture_output=True,
+            text=True,
+            check=True
+        )
+        serial_number = result.stdout.strip()
+        return serial_number
+    except subprocess.CalledProcessError as e:
+        return f"Error: {e.stderr.strip()}"
+
+with open("/tmp/perf_output.txt", "r") as f:
+    powerData = f.read()
+fileName = get_dell_serial_linux()
+
+url = f"https://api.github.com/repos/Codemeister14/HEPscoreData/contents/{fileName}Power.txt"
+headers = {"Authorization": f"token {token}"}
+data = {
+    "message": "commited",
+    "content": base64.b64encode(powerData.encode()).decode(),
+    "branch": "main",
+}
+
+res = requests.put(url, headers=headers, json=data)
+res.raise_for_status()
+print(" File committed")
